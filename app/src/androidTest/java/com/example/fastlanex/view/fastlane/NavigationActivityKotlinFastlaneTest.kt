6015828d099e6ed1a3f67eb0b95001bd480d3586
Chris Paiano <chris.paiano@gmail.com>
2018-12-06 16:29:39 -0800

    Added espresso code before screenshot to prevent intermittent blank screenshot

--- app/src/androidTest/java/com/example/fastlanex/view/fastlane/NavigationActivityKotlinFastlaneTest.kt
+++ app/src/androidTest/java/com/example/fastlanex/view/fastlane/NavigationActivityKotlinFastlaneTest.kt
@@ -1,8 +1,13 @@
 package com.example.fastlanex.view.fastlane
 
+import android.support.test.espresso.Espresso
+import android.support.test.espresso.assertion.ViewAssertions
+import android.support.test.espresso.matcher.ViewMatchers
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import com.example.fastlanex.view.activities.NavigationActivity
+import org.hamcrest.Matchers
+import org.hamcrest.core.StringContains
 import org.junit.BeforeClass
 import org.junit.ClassRule
 import org.junit.Rule
@@ -21,6 +26,14 @@ class NavigationActivityKotlinFastlaneTest {
 
     @Test
     fun testTakeScreenshot() {
+        val textView6 = Espresso.onView(
+            Matchers.allOf(
+                ViewMatchers.withText(StringContains("oobe")),
+                ViewMatchers.isDisplayed()
+            )
+        )
+        textView6.check(ViewAssertions.matches(ViewMatchers.withText(StringContains("oobe"))))
+
         Screengrab.screenshot("testScreenshotKotlin")
     }
 
